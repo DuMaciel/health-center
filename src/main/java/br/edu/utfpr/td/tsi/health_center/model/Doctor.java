db99d2b3d28b78cace3d89bb329fe63010b6a50f
FellipeAltran <altranfellipe@gmail.com>
2024-11-23 10:54:54 -0300

    add doctor class

---
+++ src/main/java/br/edu/utfpr/td/tsi/health_center/model/Doctor.java
@@ -0,0 +1,40 @@
+package br.edu.utfpr.td.tsi.health_center.model;
+
+public class Doctor {
+	private int id;
+	private String name;
+	private String cpf;
+	private String crm;
+	private Address address;
+	
+	public Doctor(int id, String name, String cpf, String crm, Address address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.crm = crm;
+		this.address = address;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getCrm() {
+		return crm;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+	
+	
+}
